9b0b1c6c8a2d0ef7774838856ee240c01ee3aeae
Matei Velicu <mattivelicu@gmail.com>
2025-11-20 20:37:46 +0200

    first session

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,52 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello to java!");
 
+        //Variabile
+        int age = 29;
+        short age2 = 30;
+        long number = 22200000;
+        double weight = 115.0;
+        float weightFloat = 115.5f;
+        boolean alive = true;
+        char litera = 'A';
+        byte something = 0;
+
+        System.out.println("voi printa varsta mea: " + age);
+
+        System.out.println("voi printa age2, nush ce e asta: " + age2);
+
+        System.out.println("voi printa acum number:" + number );
+
+        System.out.println("cantaresc: " + weight);
+
+        System.out.println("cantaresc mai exact: " + weightFloat);
+
+        System.out.println("sunt in viata :" + alive);
+
+        System.out.println("prima litera din alfabet este: " + litera);
+
+        System.out.println("printez byte: " + something);
+
+
+        //facem stringuri acu
+        String name = "Matei";
+        System.out.println("ma numesc: " + name);
+
+
+        //ne jucam cu operatori
+        int nr1 = 10;
+        int nr2 = 5;
+        int suma = 0;
+
+        suma = nr1 + nr2;
+
+        System.out.println("suma este: " + suma);
+        System.out.println(nr1-nr2);
+
+        int sub = nr1/nr2;
+        System.out.println("sub: " + sub);
         }
     }
 // heheheha
